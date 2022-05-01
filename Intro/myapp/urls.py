5515128be3145e3a07dfe37c5e8cf915e311d12f
Zainab Lawal <zainablaw1012@gmail.com>
2022-05-01 23:50:02 +0300

    extending templates

--- Intro/myapp/urls.py
+++ Intro/myapp/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns=[
     path('', views.home, name= "index"),
     path('add/<int:a>/<int:b>',views.add, name="add"),
-    path('intro/<str:name>/<int:age>', views.intro, name="intro")
+    path('intro/<str:name>/<int:age>', views.intro, name="intro"),
+    path('second', views.second, name="second")
 ]
